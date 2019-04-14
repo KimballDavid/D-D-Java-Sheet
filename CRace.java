0f33c52ee1560cc3b3e51dd233f59f518b85ec60
KimballDavid <kimbadp@gmail.com>
2019-04-14 15:38:21 -0700

    first commit

---
+++ CRace.java
@@ -0,0 +1,17 @@
+/*
+Property of David Kimball, Student National University
+*/
+
+import java.util.Scanner;
+
+public class CRace
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        
+        
+        
+    }
+}
---
+++ Character.java
@@ -0,0 +1,22 @@
+/*
+Property of David Kimball
+*/
+
+import java.util.Scanner;
+
+public class Character
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        private int str;
+        private int dex;
+        private int con;
+        private int intel;
+        private int wis;
+        private int cha;
+        
+        
+    }
+}
